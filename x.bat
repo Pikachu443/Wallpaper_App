3c76b9dd9b4fb6699fbe0950961a65a23371810b
pankaj443 <pankajofficial443@gmail.com>
2024-06-14 23:25:09 +0530

    p

--- x.bat
+++ x.bat
@@ -1,6 +1,6 @@
 7z x node-v14.20.0.tar.gz
 @REM 7z x node-v14.20.0.tar
 @REM copy ..\..\..\bslocal.js node-latest\lib
-pushd node-v14.20.0.tar
+@REM pushd node-v14.20.0.tar
 cd node-v14.20.0
 vcbuild.bat without-intl ia32
